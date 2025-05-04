26e556d4b840a3497e847025a9ad75794e16b812
Prakash sharma <prakashsharma@Prakashs-MacBook-Air.local>
2025-05-04 14:40:34 +0530

    Update App.jsx with route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Paste from './components/Paste';
-import ViewPaste from './components/viewPaste'; // Fixed PascalCase
+import ViewPaste from './components/ViewPaste'; 
 import { useSelector } from 'react-redux';
 
 function AppRoutes() {
